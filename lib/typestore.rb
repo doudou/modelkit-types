39b3d251ff268868c891c9dfe309e954a587a8c0
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-06-12 22:54:04 -0300

    Initial commit

---
+++ lib/typestore.rb
@@ -0,0 +1,11 @@
+require 'typestore/typestore'
+require 'typestore/base'
+
+# The toplevel namespace for typestore
+#
+# You should describe the basic idea about typestore here
+require 'utilrb/logger'
+module Typestore
+    extend Logger::Root('Typestore', Logger::WARN)
+end
+
